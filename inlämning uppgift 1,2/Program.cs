3e949f512bb2e172d4d94e3c188d99e623ea41ec
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-09-12 14:38:27 +0200

    Add project files.

---
+++ inlämning uppgift 1,2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("det finns ett allvarligt fel");
+        }
+    }
+}
+
